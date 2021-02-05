cf639912e78b6550833135e4f7a17e7a391a8939
Hwan-seok <tttkhs@naver.com>
2021-02-06 00:17:03 +0900

    update lisence

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,1 +1,4 @@
-
+/// Define App ID and Token
+const APP_ID = "5edfffb2550e4b99be8d2eec76bf0855";
+const Token =
+    "0065edfffb2550e4b99be8d2eec76bf0855IAAb1a6wgwZv3rA8x/2VlsOSAE4XTkV1UU6ZCTBdoXZE7Ax+f9gAAAAAEABwjq6PW6seYAEAAQBbqx5g";
